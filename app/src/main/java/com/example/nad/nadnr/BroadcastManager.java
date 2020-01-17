151e73c9b9a500d40d042dc411b9283048af2677
nadtata <nadtaetae@gmail.com>
2020-01-17 16:04:50 +0900

    test1

--- app/src/main/java/com/example/nad/nadnr/BroadcastManager.java
+++ app/src/main/java/com/example/nad/nadnr/BroadcastManager.java
@@ -93,7 +93,7 @@ public class BroadcastManager {
         callServerReceiver(mContext);
 
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
